436a228864f0b392000bafb67f81af527cce7f92
doritossban <155601706+doritossban@users.noreply.github.com>
2024-01-07 19:45:38 +0900

    Title: 변수 값 복사하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 240107/변수 값 복사하기/copying-variable-values.java
+++ 240107/변수 값 복사하기/copying-variable-values.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-       int a = 5, b = 3, c = 9;
-
+       int a = 1, b = 2, c = 3;
+       
        a = b = c;
-       System.out.println("A is " + a + " B is " + b + " C is " + c);
+       System.out.print(a+" ");
+       System.out.print(b+" " );
+       System.out.print(c+" " );
+
     }
 }

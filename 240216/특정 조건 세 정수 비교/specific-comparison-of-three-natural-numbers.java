b12f6ec886a4230f7978f82537729f3781badf64
doritossban <155601706+doritossban@users.noreply.github.com>
2024-02-16 12:46:19 +0900

    Title: 특정 조건 세 정수 비교, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240216/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 240216/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -8,16 +8,16 @@ public class Main {
         int c = sc.nextInt();
 
         if(a <= b && a <= c) {
-            System.out.println("1");
+            System.out.print("1 ");
         }
         else {
-            System.out.println("0");
+            System.out.print("0 ");
         }
         if(a == b && a==c) {
-            System.out.println("1");
+            System.out.print("1 ");
         }
         else {
-            System.out.println("0");
+            System.out.print("0 ");
         }
 
     }

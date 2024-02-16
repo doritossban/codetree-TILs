a1491c1240adf9ac377ebb2d8d4b16885a232c9b
doritossban <155601706+doritossban@users.noreply.github.com>
2024-02-16 17:15:31 +0900

    Title: 둘 중 하나의 배수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240216/둘 중 하나의 배수/multiple-of-either.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if(a%3 && a%5){
+            System.out.println("1");
+        }
+        else {
+            System.out.println("0");
+        }
+
+    
+    }
+}

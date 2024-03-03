45689db9a4b5e19f07db4bd3e7789cd604546e59
doritossban <155601706+doritossban@users.noreply.github.com>
2024-03-03 22:27:41 +0900

    Title: 두 사람, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240303/두 사람/two-person.java
+++ 240303/두 사람/two-person.java
@@ -10,11 +10,13 @@ public class Main {
         int b1 = sc.nextInt();
         char b2 = sc.next().charAt(0);
 
-    if ( a2 == 'W' || b2 == 'W') {
-        if( a1 < 19 && b1 < 19) {
+    if ( a2 == 'W' && b2 == 'W') {
         System.out.println('0');
-        }
     }
+    else if( a1 < 19 && b1 < 19) {
+        System.out.println('0');
+        } 
+
     else {
         System.out.println('1');
     }

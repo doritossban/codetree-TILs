878cc19945c793d0a8caa21eed89814c2a627347
doritossban <155601706+doritossban@users.noreply.github.com>
2024-03-05 00:08:13 +0900

    Title: 윤년인가, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 240305/윤년인가/is-leap-year.java
+++ 240305/윤년인가/is-leap-year.java
@@ -6,14 +6,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
 
-        if (a % 4 == 0) {
-            System.out.println("true");
-            
-        }
-        else if (a % 100 ==0 && a % 400 != 0) {
+        if (a % 100 == 0 && a % 400 != 0) {
             System.out.println("false");
 
         }
+        else if (a % 4 == 0) {
+            System.out.println("true");
+            
+        }
+        
         else {
             System.out.println("false");
         }

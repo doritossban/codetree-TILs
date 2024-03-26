5f014a862f7e0295f53b16c2cb452cad68a2f334
doritossban <155601706+doritossban@users.noreply.github.com>
2024-03-26 13:37:52 +0900

    Title: 증가시키며 출력하기 3, Time: 106ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240326/증가시키며 출력하기 3/increase-and-print-3.java
+++ 240326/증가시키며 출력하기 3/increase-and-print-3.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 10;
+        int i = 5;
 
-        while (i <= 26) {
-            System.out.print(i+" ");
+        while (i <= 10) {
+            System.out.println(i);
             i += 2;
         }
     }

bc4adfc84024907553553e956df73fb1a4e1892c
doritossban <155601706+doritossban@users.noreply.github.com>
2024-05-18 15:02:48 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240518/369 게임/369-game.java
+++ 240518/369 게임/369-game.java
@@ -1,34 +1,20 @@
 import java.util.Scanner;
 
-
-
 public class Main {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        int n = sc.nextInt();
 
-        for (int i = 1; i <= a; i++) {
-            if ( i % 30 == 0) {
-            for (int j = 0; j == 9; j++) {
-            System.out.print("0" + " ");
-            i++;
-            }
-            
-        }
-        if ( i % 3 == 0 ) {
-            System.out.print('0' + " ");
-        }
-        else if ( (i%10)%3 == 0 ) {
-            System.out.print('0' + " ");
-        }
-        else {
-        
-            System.out.print(i + " ");
-        }
-
-        
-
+        for(int i = 1; i <= n; i++) {
+            if(i % 3 == 0)
+            System.out.print("0 ")'
+            else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+                System.out.print("0 ");
+            else if(i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
+                System.out.print("0 ");
+            else
+                System.out.print(i + " ");
         }
 
     }

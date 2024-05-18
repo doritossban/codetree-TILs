d472d3bf4c84422ce44a0eba77bd8c7e03a89cda
doritossban <155601706+doritossban@users.noreply.github.com>
2024-05-18 14:49:48 +0900

    Title: 369 게임, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240518/369 게임/369-game.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            if ( i % 30 == 0) {
+            for (int j = 0; j == 9; j++) {
+            System.out.print("0" + " ");
+            }
+            i++;
+        }
+        if ( i % 3 == 0 ) {
+            System.out.print('0' + " ");
+        }
+        else if ( (i%10)%3 == 0 ) {
+            System.out.print('0' + " ");
+        }
+        else {
+        
+            System.out.print(i + " ");
+        }
+
+        
+
+        }
+
+    }
+}

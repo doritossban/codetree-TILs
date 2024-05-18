9b712c4a7d1c6d8201dae05a2e9c1295a6ac86a5
doritossban <155601706+doritossban@users.noreply.github.com>
2024-05-18 15:41:52 +0900

    Title: 배수의 개수, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240518/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {    
+        Scanner sc = new Scanner(System.in);
+
+        int a, cnt3 = 0, cnt5 = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            a = sc.nextInt();
+
+            if(a % 3 == 0) 
+                cnt3++;
+            if(a % 5 == 0)
+                cnt5++;
+        }
+
+        System.out.print(cnt3 + " " + cnt5);
+
+    }
+}

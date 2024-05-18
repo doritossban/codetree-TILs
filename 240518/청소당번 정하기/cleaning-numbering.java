b5088dfdd7ed116d9d9a2cbc477afbb3832d8a9b
doritossban <155601706+doritossban@users.noreply.github.com>
2024-05-18 16:09:37 +0900

    Title: 청소당번 정하기, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240518/청소당번 정하기/cleaning-numbering.java
+++ 240518/청소당번 정하기/cleaning-numbering.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cnt1,cnt2,cnt3 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
+        int cnt12 = 0;
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
             if (i % 12 == 0) {
-                cnt3++;
+                cnt12++;
             }
             else if (i % 3 == 0) {
-                cnt1++;
+                cnt3++;
             }
             else if (i % 2 == 0) {
                 cnt2++;
@@ -19,5 +21,7 @@ public class Main {
             
         }
 
+
+            System.out.println(cnt2 + " " + cnt3 + " " + cnt12);
     }
 }

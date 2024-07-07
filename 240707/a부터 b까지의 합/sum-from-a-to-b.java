6735936c562636589106b2b6fc629ad2f105deff
doritossban <155601706+doritossban@users.noreply.github.com>
2024-07-07 11:30:10 +0900

    Title: a부터 b까지의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240707/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int sumVal = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sumVal += i;
+            
+            }
+        }
+        System.out.println(sumVal);
+
+        }
+}

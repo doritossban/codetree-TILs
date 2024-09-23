7323ede506029e312bc8e8d2c5af844f54fed3a3
doritossban <155601706+doritossban@users.noreply.github.com>
2024-09-23 16:10:43 +0900

    Title: 1까지 나누기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240923/1까지 나누기/divide-by-1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int i = 1;
+        int num = 0;
+
+        while(true) {
+            n / i = num;  
+            //int 형 자료이기 때문에
+           // System.out.print(n);
+            System.out.print(num);
+
+
+        }    
+        
+    }
+}
